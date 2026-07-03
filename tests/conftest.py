cc70a23f5902e48b0656f2ec6bac394ea04e341b
huangjiasen <huangjiasen@xiaoyingai.com>
2026-07-03 18:10:18 +0800

    test: provide dummy OpenAI key in CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,7 @@ from __future__ import annotations
 
 import os
 
+if not os.environ.get("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = "test-key"
+
 os.environ.setdefault("OPENAI_AGENTS_DISABLE_TRACING", "true")
